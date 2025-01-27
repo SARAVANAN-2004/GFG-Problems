49a0c8e46b8d0b166a1006e24a8143e6f7ce6bca
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-01-27 21:34:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/LRU Cache/lru-cache.java
@@ -0,0 +1,120 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class LRUDesign {
+
+    private static List<String> inputLine(Scanner sc) {
+        return Arrays.asList(sc.nextLine().split(" "));
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+
+        while (t-- > 0) {
+            int capacity = Integer.parseInt(sc.nextLine());
+            LRUCache cache = new LRUCache(capacity);
+
+            int queries = Integer.parseInt(sc.nextLine());
+            while (queries-- > 0) {
+                List<String> vec = inputLine(sc);
+                if (vec.get(0).equals("PUT")) {
+                    int key = Integer.parseInt(vec.get(1));
+                    int value = Integer.parseInt(vec.get(2));
+                    cache.put(key, value);
+                } else {
+                    int key = Integer.parseInt(vec.get(1));
+                    System.out.print(cache.get(key) + " ");
+                }
+            }
+            System.out.println();
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// design the class in the most optimal way
+class Node {
+    int key;
+    int value;
+    Node next;
+    Node prev;
+
+    Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class LRUCache {
+    private int capacity;
+    private Map<Integer, Node> cacheMap;
+    private Node head;
+    private Node tail;
+
+    // Constructor to initialize the cache with a given
+    // capacity
+    LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.cacheMap = new HashMap<>();
+        this.head = new Node(-1, -1);
+        this.tail = new Node(-1, -1);
+        this.head.next = this.tail;
+        this.tail.prev = this.head;
+    }
+
+    // Function to get the value for a given key
+    int get(int key) {
+        if (!cacheMap.containsKey(key)) {
+            return -1;
+        }
+
+        Node node = cacheMap.get(key);
+        remove(node);
+        add(node);
+        return node.value;
+    }
+
+    // Function to put a key-value pair into the cache
+    void put(int key, int value) {
+        if (cacheMap.containsKey(key)) {
+            Node oldNode = cacheMap.get(key);
+            remove(oldNode);
+        }
+
+        Node node = new Node(key, value);
+        cacheMap.put(key, node);
+        add(node);
+
+        if (cacheMap.size() > capacity) {
+            Node nodeToDelete = tail.prev;
+            remove(nodeToDelete);
+            cacheMap.remove(nodeToDelete.key);
+        }
+    }
+
+    // Add a node right after the head (most recently used
+    // position)
+    private void add(Node node) {
+        Node nextNode = head.next;
+        head.next = node;
+        node.prev = head;
+        node.next = nextNode;
+        nextNode.prev = node;
+    }
+
+    // Remove a node from the list
+    private void remove(Node node) {
+        Node prevNode = node.prev;
+        Node nextNode = node.next;
+        prevNode.next = nextNode;
+        nextNode.prev = prevNode;
+    }
+}
