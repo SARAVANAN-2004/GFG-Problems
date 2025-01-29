3ab16de83f390e3c4d37fe00fbe3ad320cd9932c
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-01-29 19:05:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Implement Pow/implement-pow.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.text.DecimalFormat;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            double b = sc.nextDouble();
+            int e = sc.nextInt();
+            Solution ob = new Solution();
+            System.out.printf("%.5f\n", ob.power(b, e));
+            System.out.println("~");
+        }
+        sc.close();
+    }
+}
+
+
+// } Driver Code Ends
+// User function Template for Java
+class Solution {
+    double power(double b, int e) {
+        // code here
+        double val = b;
+        if(e == 0) return 1;
+        val = Math.pow(b,Math.abs(e));
+        
+        if(e < 0){
+            return 1/val;
+        }
+        return val;
+    }
+}
+
+//{ Driver Code Starts.
+// } Driver Code Ends
