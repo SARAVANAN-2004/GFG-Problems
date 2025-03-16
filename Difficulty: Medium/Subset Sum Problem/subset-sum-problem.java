4d6aa7a5bc9daab897b3799b60b9c0d6ace8387c
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2025-03-17 00:24:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int N = input_line.length;
+            int arr[] = new int[N];
+            for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(input_line[i]);
+            int sum = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            if (ob.isSubsetSum(arr, sum))
+                System.out.println("true");
+            else
+                System.out.println("false");
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+
+    static Boolean isSubsetSum(int arr[], int sum) {
+        // code here
+        return solve(0,arr,sum,0);
+    }
+    
+    static boolean solve(int i,int[] arr,int sum,int cur){
+        if(cur == sum) return true;
+        
+        if(cur > sum || i >= arr.length) return false;
+        
+        return solve(i+1,arr,sum,cur+arr[i]) || solve(i+1,arr,sum,cur);
+    }
+    
+    
+}
